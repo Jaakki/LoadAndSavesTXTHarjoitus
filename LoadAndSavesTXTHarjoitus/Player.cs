9616c361e83f5d5d81357a1dadf22b29267c2984
Jaakki <62286646+Jaakki@users.noreply.github.com>
2020-04-23 14:39:35 +0300

    Nyt tapahtuu jotain ihan diiba daabaa O.o

--- LoadAndSavesTXTHarjoitus/Player.cs
+++ LoadAndSavesTXTHarjoitus/Player.cs
@@ -24,18 +24,18 @@ namespace LoadAndSavesTXTHarjoitus
             while (string.IsNullOrEmpty(player.Name))
             {
                 Console.WriteLine("Invalid name given!\nEnter a new name or exit with 0");
-                if (Console.ReadKey().Key == ConsoleKey.D0)
+                if (Console.ReadKey(false).Key == ConsoleKey.D0)
                 {
                     goto BackToMenu;
                 }
                 player.Name = Console.ReadLine();
             }
             Console.WriteLine("Starting experience: ");
-            validateString = (Console.ReadLine()+0);
+            validateString = (Console.ReadLine());
             while (!int.TryParse(validateString, out invalidInt))
             {
                 Console.WriteLine("Invalid value!\nEnter a new value or exit with 0");
-                if (Console.ReadKey().Key == ConsoleKey.D0)
+                if (Console.ReadKey(false).Key == ConsoleKey.D0)
                 {
                     goto BackToMenu;
                 }
@@ -43,11 +43,11 @@ namespace LoadAndSavesTXTHarjoitus
             }
             player.EXP = Convert.ToInt32(validateString);
             Console.WriteLine("Starting gold: ");
-            validateString = (Console.ReadLine() + 0);
+            validateString = (Console.ReadLine());
             while (!int.TryParse(validateString, out invalidInt))
             {
                 Console.WriteLine("Invalid value!\nEnter a new value or exit with 0");
-                if (Console.ReadKey().Key == ConsoleKey.D0)
+                if (Console.ReadKey(false).Key == ConsoleKey.D0)
                 {
                     goto BackToMenu;
                 }
@@ -62,10 +62,16 @@ namespace LoadAndSavesTXTHarjoitus
 
         public static void DisplayPlayers(List<Player> players)             //prints player info in this format
         {
+            if (players.Count == 0)
+            {
+                Console.WriteLine("No players in list!");
+                goto BackToMenu;
+            }
             foreach (Player player in players)
             {
                 Console.WriteLine($"Player name: {player.Name} - Experience: {player.EXP} - Player Gold: {player.Gold} - Player is alive? {player.IsAlive}");
             }
+            BackToMenu:
             Console.ReadLine();
         }
     }
